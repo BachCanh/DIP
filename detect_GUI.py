dd4b22128a5c7d867918db00e39e54ab4cf2293f
canhhandsome <cbd1708@gmail.com>
2025-05-09 20:09:38 +0700

    add process

--- detect_GUI.py
+++ detect_GUI.py
@@ -1,4 +1,10 @@
 import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+import torch
+torch.set_num_threads(1)
 import cv2
 import time
 import threading
